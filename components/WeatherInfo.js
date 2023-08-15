0c7cf825bc61ec3aafc12d5be523ff5e4e4746aa
Ali Kozan <ankozan@mail.fhsu.edu>
2023-08-14 21:16:59 -0700

    character added

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -1,25 +1,49 @@
-// TestComponent.js
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const WeatherInfo = () => {
     return (
         <View style={styles.container}>
-            <Text>Puyallup</Text>
-            <Text>Hazy</Text>
-            <Text>Feels Like 88</Text>
+            <Text style={styles.cityName}>Puyallup</Text>
+            <Text style={styles.condition}>Hazy</Text>
+            <Text style={styles.temperature}>84°F</Text>
+            <Text style={styles.feelsLike}>Feels Like 88°F</Text>
         </View>
     );
 };
+
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
         alignItems: 'center',
         position: 'absolute',
-        backgroundColor: 'rgba(255, 255, 255, 0.7)', // Semi-transparent background
-        top: "10%"
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        borderRadius: 10,
+        padding: 20,
+        width: 250,
+        top: '5%',
+
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        zIndex: 10,
+    },
+    cityName: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    condition: {
+        fontSize: 18,
+        marginBottom: 5,
+    },
+    temperature: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    feelsLike: {
+        fontSize: 16,
     },
 });
-
 
 export default WeatherInfo;
--- screens/FirstPage.js
+++ screens/FirstPage.js
@@ -1,12 +1,17 @@
 // FirstPage.js
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, View, Text, StyleSheet } from 'react-native';
 import WeatherInfo from '../components/WeatherInfo';
 
 const FirstPage = () => {
     return (
         <View style={styles.container}>
             <WeatherInfo />
+            <Image
+                source={require('../assets/man2.png')}
+                style={styles.image}
+                resizeMode="contain"
+            />
         </View>
     );
 };
@@ -15,6 +20,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    image: {
+        width: 525, // Adjust the image width as needed
+        height: 800, // Adjust the image height as needed
     },
 });
 
